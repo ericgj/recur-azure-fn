237d5ab14665ea0003b34e20d4e7c014c17dc92d
Eric Gjertsen <eric.gjertsen@clario.com>
2023-03-06 13:13:33 -0500

    reimpl parsing for custom types not dealt with directly by library
    Note: these are old changes from March 2022 never committed to git.

--- expand/__init__.py
+++ expand/__init__.py
@@ -1,11 +1,42 @@
 from datetime import datetime
+from enum import Enum
 import json
 import logging
+from typing import List
 
 import azure.functions as func  # type: ignore
 from dateutil.rrule import rrule
+from dateutil.rrule import MO, TU, WE, TH, FR, SA, SU, MONTHLY, WEEKLY
 
 MAX_COUNT = 1000
+WEEKDAYS = [MO,TU,WE,TH,FR]
+
+class FreqValues(Enum):
+    WEEKLY = "weeks"
+    MONTHLY = "months"
+
+class MonthTypeValues(Enum):
+    DAYS_OF_WEEK = "days of week"
+    DAYS_OF_MONTH = "days of month"
+    FIRST_WEEKDAY = "first weekday"
+    LAST_WEEKDAY = "last weekday"
+
+class WeekDayValues(Enum):
+    MONDAY = "monday"
+    TUESDAY = "tuesday"
+    WEDNESDAY = "wednesday"
+    THURSDAY = "thursday"
+    FRIDAY = "friday"
+    SATURDAY = "saturday"
+    SUNDAY = "sunday"
+
+class MonthNthDayValues(Enum):
+    FIRST = "1st"
+    SECOND = "2nd"
+    THIRD = "3rd"
+    FOURTH = "4th"
+    LAST = "last"
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     raw_body = req.get_body().decode('utf-8','ignore')
@@ -39,27 +70,157 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     
 
 def parse_rrule(body) -> rrule:
-    freq = body.get('freq',None)
-    start = body.get('start',None)
-    interval = int(body.get('interval',1))
-    until = body.get('until',None)
-    if freq is None:
+    raw_freq = body.get('freq',None)
+    raw_start = body.get('start',None)
+    raw_interval = body.get('interval',1)
+    raw_until = body.get('until',None)
+    raw_month_type = body.get('month_type',None)
+    raw_month_nth_days = body.get('month_nth_days',[])
+    raw_month_days = body.get('month_days',[])
+    raw_week_days = body.get('week_days',[])
+
+    if raw_freq is None:
         raise ValueError('Missing required: freq')
-    if start is None:
+    if raw_start is None:
         raise ValueError('Missing required: start')
-    return rrule(
-        freq=parse_freq(freq),
-        dtstart=parse_datetime(start),
-        interval=interval,
-        until=None if until is None else parse_datetime(until),
-    )
+    
+    freq = parse_freq(raw_freq)
+    start = parse_datetime(raw_start)
+    interval = int(raw_interval)
+    until = None if raw_until is None else parse_datetime(raw_until)
+
+    if freq == WEEKLY:
+        week_days = parse_week_days(raw_week_days)
+        return rrule(
+            freq=freq,
+            dtstart=start,
+            interval=interval,
+            until=until,
+            byweekday=week_days,
+        )
+
+    if freq == MONTHLY:
+        if raw_month_type is None:
+            raise ValueError('Missing required: month_type')
+
+        month_type = parse_month_type(raw_month_type)
+
+        if month_type == MonthTypeValues.DAYS_OF_MONTH:
+            month_days = parse_month_days(raw_month_days)
+            return rrule(
+                freq=freq,
+                dtstart=start,
+                interval=interval,
+                until=until,
+                bymonthday=month_days,
+            )
+
+        if month_type == MonthTypeValues.DAYS_OF_WEEK:
+            month_nth_days = parse_month_nth_days(raw_month_nth_days)
+            week_days = parse_week_days(raw_week_days)
+            return rrule(
+                freq=freq,
+                dtstart=start,
+                interval=interval,
+                until=until,
+                bysetpos=month_nth_days,
+                byweekday=week_days,
+            )
+            
+        if month_type == MonthTypeValues.FIRST_WEEKDAY:
+            return rrule(
+                freq=freq,
+                dtstart=start,
+                interval=interval,
+                until=until,
+                bysetpos=1,
+                byweekday=WEEKDAYS,
+            )
+
+        if month_type == MonthTypeValues.LAST_WEEKDAY:
+            return rrule(
+                freq=freq,
+                dtstart=start,
+                interval=interval,
+                until=until,
+                bysetpos=-1,
+                byweekday=WEEKDAYS,
+            )
+
+        raise ValueError('Month recurrence type not implemented: "{month_type}"')
+
+    raise ValueError('Frequency not implemented: "{freq}"')
+
+
 
 def parse_freq(s: str):
     try:
-        return ['years','months','weeks'].index(s)
-    except IndexError:
+        freq = FreqValues(s)
+    except ValueError:
         raise ValueError(f'Unknown freq: "{s}"')
+    if freq == freq.WEEKLY:
+        return WEEKLY
+    if freq == freq.MONTHLY:
+        return MONTHLY
 
+    raise ValueError(f'Unknown freq: "{s}"')
+
+def parse_week_days(raw: List[str]):
+    return [ parse_week_day(s,i) for (i,s) in enumerate(raw) ]
+
+def parse_week_day(s: str, i: int):
+    try:
+        week_day = WeekDayValues(s.lower())
+    except ValueError:
+        raise ValueError(f'Unknown weekday: "{s}" ({i})')
+    if week_day == WeekDayValues.MONDAY:
+        return MO
+    if week_day == WeekDayValues.TUESDAY:
+        return TU
+    if week_day == WeekDayValues.WEDNESDAY:
+        return WE
+    if week_day == WeekDayValues.THURSDAY:
+        return TH
+    if week_day == WeekDayValues.FRIDAY:
+        return FR
+    if week_day == WeekDayValues.SATURDAY:
+        return SA
+    if week_day == WeekDayValues.SUNDAY:
+        return SU
+
+    raise ValueError(f'Unknown weekday: "{s}" ({i})')
+
+def parse_month_type(s: str):
+    try:
+        return MonthTypeValues(s.lower())
+    except ValueError:
+        raise ValueError(f'Unknown month type: "{s}"')
+
+def parse_month_days(raw: List[str]):
+    return [ int(s) for s in raw ]
+
+def parse_month_nth_days(raw: List[str]):
+    return [ parse_month_nth_day(s,i) for (i,s) in enumerate(raw) ]
+
+def parse_month_nth_day(s: str, i: int):
+    try:
+        month_nth_day = MonthNthDayValues(s)
+    except ValueError:
+        raise ValueError(f'Unknown Month nth value: "{s}" ({i})')
+    if month_nth_day == MonthNthDayValues.FIRST:
+        return 1
+    if month_nth_day == MonthNthDayValues.SECOND:
+        return 2
+    if month_nth_day == MonthNthDayValues.THIRD:
+        return 3
+    if month_nth_day == MonthNthDayValues.FOURTH:
+        return 4
+    if month_nth_day == MonthNthDayValues.LAST:
+        return -1
+
+    raise ValueError(f'Unknown Month nth value: "{s}" ({i})')
+
+ 
 # TODO: timezone
 
 def parse_datetime(s: str) -> datetime:
